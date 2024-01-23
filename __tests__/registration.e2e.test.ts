61a5723695bdfee2db2e3117910c5c31898aae76
AMayfet <amayfet@alfabank.ru>
2024-01-23 21:06:44 +0300

    feat: added extra auth/reg flow tests

--- __tests__/registration.e2e.test.ts
+++ __tests__/registration.e2e.test.ts
@@ -8,6 +8,17 @@ import { App } from '../src/app'
 
 const { mock } = nodemailer as unknown as NodemailerMock
 let application: App
+const email = 'test@mail.com'
+const login = 'ulogin45'
+let code: string
+
+const getCode = (html: string) => {
+  const $ = load(html)
+  const link = $('a').attr('href')
+  const url = new URL(link!)
+  const queryParams = new URLSearchParams(url.search)
+  return queryParams.get('code')!
+}
 
 beforeAll(async () => {
   const { app } = boot
@@ -17,28 +28,69 @@ beforeAll(async () => {
   await request(application.app).delete('/testing/all-data').expect(204)
 })
 
+afterEach(async () => {
+  mock.reset()
+})
+
 describe('Registration', () => {
   it('POST -> "auth/registration": should create a new user and send a confirmation email with a code', async () => {
-    const email = 'test@mail.com'
-
     const res = await request(application.app).post('/auth/registration').send({
       email,
-      login: 'ulogin45',
+      login,
       password: 'ulogin45',
     })
 
     const sentEmails = mock.getSentMail()
     const html = sentEmails[0].html as string
-    const $ = load(html)
-    const link = $('a').attr('href')
-    const url = new URL(link!)
-    const queryParams = new URLSearchParams(url.search)
-    const code = queryParams.get('code')
+    code = getCode(html)
 
     expect(sentEmails.length).toBe(1)
     expect(res.status).toBe(204)
     expect(sentEmails[0].to).toBe(email)
     expect(code).toBeTruthy()
+  })
+
+  it('POST -> "/auth/registration": should return error if email or login already exist; status 400', async () => {
+    await request(application.app)
+      .post('/auth/registration')
+      .send({
+        email,
+        login,
+        password: 'password123',
+      })
+      .expect(400)
+  })
+
+  it('POST -> "/auth/registration-email-resending": should send email with new code if user exists but not confirmed yet; status 204', async () => {
+    const res = await request(application.app)
+      .post('/auth/registration-email-resending')
+      .send({
+        email,
+      })
+
+    const sentEmails = mock.getSentMail()
+    const html = sentEmails[0].html as string
+    code = getCode(html)
+
+    expect(res.status).toBe(204)
+  })
+
+  it('POST -> "/auth/registration-confirmation": should confirm registration by email; status 204', async () => {
+    await request(application.app)
+      .post('/auth/registration-confirmation')
+      .send({
+        code,
+      })
+      .expect(204)
+  })
+
+  it('POST -> "/auth/registration-confirmation": should return error if code already confirmed; status 400', async () => {
+    await request(application.app)
+      .post('/auth/registration-confirmation')
+      .send({
+        code,
+      })
+      .expect(400)
   })
 })
 
